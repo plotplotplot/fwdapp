a367d887cfb840d9c175185c1cef4686cfb4e4f6
Jeanneveev <gjjones5425@gmail.com>
2026-05-18 12:56:11 -0400

    added functionality for loading talk info

--- peptalks/talk_crud/create_talk.js
+++ peptalks/talk_crud/create_talk.js
@@ -1,73 +1,136 @@
-class Attendee {
-    constructor(first_name, last_name, email, role) {
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.email = email;
-        this.role = role;
+/* NOTE:
+    Talk info = Pep talk title, description, and image
+    Attendees = Creator, co-hosts, and invitees
+    Hosts = Creator and co-hosts
+*/
+
+/* OBJECT DEFINITIONS */
+// NOTE: Maybe move to own file?
+class Role {
+    static CREATOR = "creator";
+    static CO_HOST = "co-host";
+    static INVITEE = "invitee";
+
+    static is_valid(role) {
+        return [Role.CREATOR, Role.CO_HOST, Role.INVITEE].includes(role);
     }
 }
+/**
+ * @typedef {Object} Attendee
+ * @property {string} first_name
+ * @property {string} last_name
+ * @property {string} email
+ * @property {Role} role
+ */
+
+
+/* LOAD DATA */
+/**
+ * @returns {Object<string>} An object containing the talk title, description, and image
+ */
+const get_talk_info = () => {
+    // TODO: Get talk title and description
+    //  Call backend
+    
+    return {
+        "title": null,
+        "descr": null,
+        "img": null
+    };
+}
+
+const fill_talk_form = () => {
+    const title_inpt = document.getElementById("title");
+    const descr_inpt = document.getElementById("description");
+    const img = document.getElementById("image");
+
+    const talk_info = get_talk_info();
+
+    title_inpt.setAttribute("placeholder", talk_info["title"]);
+    descr_inpt.setAttribute("placeholder", talk_info["descr"]);
+    img.setAttribute("src", talk_info["img"]);
+}
+
 /**
  * @returns {Attendee[]} List of all attendees (including host and co-hosts)
  */
 const get_all_attendees = () => {
-    // TODO: Get attendee name, email, and role
+    // TODO: Get attendee names, emails, and roles
+    //  Call backend
+    //  Turn to class objects
+
+    return [];
 }
-const display_attendees = () => {
+const build_attendee_row = (table, attendee) => {
+    const row = document.createElement("tr");
+
+    // Info
+    const info_td = document.createElement("td");
+    const info = `
+        ${attendee.first_name} ${attendee.last_name}<br />
+        ${attendee.email}
+    `;
+    info_td.appendChild(info);
+
+    // Role dropdown
+    const roles = ["Host", "Co-Host", "Invitee"];
+    const role_td = document.createElement("td");
+    const role_select = document.createElement("select");
+    roles.forEach(role => {
+        const option = document.createElement("option");
+        option.value = role;
+        option.text = role;
+        if (role === attendee.role) {
+            option.selected = true;
+        }
+        role_select.appendChild(option);
+    });
+    role_td.appendChild(role_select);
+
+    // Edit button
+    const edit_td = document.createElement("td");
+    const edit_btn = document.createElement("button");
+    edit_btn.innerText = "Edit";
+    edit_td.appendChild(edit_btn);
+
+    // Message button
+    const message_td = document.createElement("td");
+    const message_btn = document.createElement("button");
+    message_btn.innerText = "Message";
+    message_td.appendChild(message_btn);
+
+    // Delete button
+    const delete_td = document.createElement("td");
+    const delete_btn = document.createElement("button");
+    delete_btn.innerText = "Delete";
+    delete_td.appendChild(delete_btn);
+
+    row.appendChild(info_td);
+    row.appendChild(role_td);
+    row.appendChild(edit_td);
+    row.appendChild(message_td);
+    row.appendChild(delete_td);
+
+    table.appendChild(row);
+}
+
+const load_display = () => {
+    // Load talk info
+    fill_talk_form();
+
+    // Load co-hosts
+    
+
+    // Load attendee table
+    const table = document.getElementById("attendee_table");
     const attendees = get_all_attendees();
     attendees.forEach(attendee => {
-        const table = document.getElementById("attendee_table");
-        const row = document.createElement("tr");
-
-        // Info
-        const info_td = document.createElement("td");
-        const info = `
-            ${attendee.first_name} ${attendee.last_name}<br />
-            ${attendee.email}
-        `;
-        info_td.appendChild(info);
-
-        // Role dropdown
-        const roles = ["Host", "Co-Host", "Invitee"];
-        const role_td = document.createElement("td");
-        const role_select = document.createElement("select");
-        roles.forEach(role => {
-            const option = document.createElement("option");
-            option.value = role;
-            option.text = role;
-            if (role === attendee.role) {
-                option.selected = true;
-            }
-            role_select.appendChild(option);
-        });
-        role_td.appendChild(role_select);
-
-        // Edit button
-        const edit_td = document.createElement("td");
-        const edit_btn = document.createElement("button");
-        edit_btn.innerText = "Edit";
-        edit_td.appendChild(edit_btn);
-
-        // Message button
-        const message_td = document.createElement("td");
-        const message_btn = document.createElement("button");
-        message_btn.innerText = "Message";
-        message_td.appendChild(message_btn);
-
-        // Delete button
-        const delete_td = document.createElement("td");
-        const delete_btn = document.createElement("button");
-        delete_btn.innerText = "Delete";
-        delete_td.appendChild(delete_btn);
-
-        row.appendChild(info_td);
-        row.appendChild(role_td);
-        row.appendChild(edit_td);
-        row.appendChild(message_td);
-        row.appendChild(delete_td);
-
-        table.appendChild(row);
+       build_attendee_row(table, attendee); 
     });
 }
+
+
+
 
 /* EVENT LISTENERS */
 /* Upload Image */
